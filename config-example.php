5ed47caedb44245937f5362a910619e219bd55e6
Pieter Hordijk <info@pieterhordijk.com>
2015-12-05 15:27:49 +0100

    Added example config

---
+++ config-example.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+/**
+ * Configuration file
+ *
+ * Copy this file to ./config.php and change to your environment variables
+ *
+ * PHP version 7.0
+ *
+ * @category   SevenPress\Metrics
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace SevenPress\Metrics;
+
+/**
+ * Setup the target WordPress directory
+ */
+$targetDirectory = '/path/to/wordpress';
